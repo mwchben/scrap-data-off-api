18b6f3aafd11372113bce1c5432e2019539ed375
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2021-11-25 13:54:51 +0300

    about to use cheerio

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app = express();
 app.get('/',(req, res)=>{
     res.json("Climate Change News");
 })
+app.get('/news',(req, res)=>{
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+    //since it's promised based(assynchronous programming)
+    .then (resp =()=>{
+        console.log("response",resp)
+    })
+})
 
 //call the router
 app.listen(PORT, (console.log(`Server running on port 8000 ${PORT}`)));
