33bc3cffa52961a8502fb92c0fe69cbdd74b5746
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2021-11-25 13:42:47 +0300

    npm run start does not start-off the server

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 app = express();
+
+app.get('/',(req, res)=>{
+    res.json("Climate Change News");
+})
+
+//call the router
+app.listen(PORT, (console.log(`Server running on port 8000 ${PORT}`)));
