e0948795fd3325dede7b34f1ffd39bf220a77f0a
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2021-11-26 20:00:36 +0300

    learned using attr and text f(x)s

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const PORT = 8000;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { html } = require('cheerio/lib/api/manipulation');
+
 
 app = express();
 
@@ -10,11 +12,18 @@ app.get('/',(req, res)=>{
 })
 app.get('/news',(req, res)=>{
     axios.get('https://www.theguardian.com/environment/climate-crisis')
-    //since it's promised based(assynchronous programming)
-    .then (resp =()=>{
-        console.log("response",resp)
+    //since it's promised based(asynchronous programming)
+    .then ((resp) => {
+        const HTML = resp.data;
+        console.log(HTML);
+        const $ = cheerio.load(HTML);
+
+        $('a:contains("climate")',html).each(function(){
+            const title = $(this).text(); // $(this) means  $('a:contains("climate")',html)
+            const URL = $(this).attr('href');
+        })
     })
 })
 
 //call the router
-app.listen(PORT, (console.log(`Server running on port 8000 ${PORT}`)));
+app.listen(PORT, (console.log(`Server running on port ${PORT}`)));
