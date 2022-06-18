3746c0637849f87fe74cbc37c73d8b453dc0bc66
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-18 10:32:55 +0300

    initial file

---
+++ multiple-sites.js
@@ -0,0 +1,38 @@
+const PORT = 8000;
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { html } = require('cheerio/lib/api/manipulation');
+
+const articles = [];
+const app = express();
+
+app.get('/',function(req, res){
+    // also res.send("Climate Change News");
+    res.json("Climate Change News");
+  
+})
+app.get('/news',(req, res)=>{ //also a way to write the .get
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+    //since it's promised based(asynchronous programming)
+    .then ((resp) => {
+        const HTML = resp.data;
+        //console.log(HTML);
+        const $ = cheerio.load(HTML);
+
+        $('a:contains("climate")',HTML).each(function(){
+            const title = $(this).text(); // $(this) means  $('a:contains("climate")',html)
+            const url = $(this).attr('href');
+
+            //in the array articles we push an object with title and url
+            articles.push({
+                title,
+                url
+            })
+            res.json(articles);
+        })
+    }).catch((err)=>(console.error(err)))
+})
+
+//call the router
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
