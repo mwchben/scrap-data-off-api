08ec70de42256b9c5cc3bd21a115630126653fb4
Benjamin Chege <benjamwa05@gmail.com>
2025-06-02 11:21:05 +0300

    redifind app server minimalistic

---
+++ website/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+
+//route var
+const PORT = process.env.PORT || 3000;
+const indexRouter = require('./routes/index.js');
+
+app.set('veiws','views')
+app.set('view engine', 'ejs')
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.use('/', indexRouter    );
+
+app.listen(PORT, () => {
+  console.log(`GUI app is running on http://localhost:${PORT}`);
+});
