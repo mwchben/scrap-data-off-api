4481db76de2b8c5ad50aac0987a783178dd781a3
Benjamin Chege <benjamwa05@gmail.com>
2025-06-07 11:42:48 +0300

    data confirmed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const router = express.Router();
+const  router = express.Router();
 
 
 router.get('/', (req, res) => {
-  res.render('index', { title: "Express" });
+  res.render('index', { title: "Scrap" });
 });
+router.post('/submit', (req, res) => {
+  const {website} = req.body
+  res.render('toServer.ejs', {website})
+})
 
 module.exports = router
