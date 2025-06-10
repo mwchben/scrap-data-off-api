db4b00e88fe61dfbd6c49ac836c29532936540fb
Benjamin Chege <benjamwa05@gmail.com>
2025-06-10 21:30:55 +0300

    modifications to index compliments to co-pilot

--- server/index.js
+++ server/index.js
@@ -1,39 +1,50 @@
 const PORT = 8000;
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { html } = require('cheerio/lib/api/manipulation');
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs"); // Add this at the top
 
 const app = express();
-const articles = [];
+var articles = [];
 
+// News scraping route
+app.get("/", (req, res) => {
+  axios
+    .get("https://www.bbc.com")
+    .then((resp) => {
+      const HTML = resp.data;
+      const $ = cheerio.load(HTML);
+      articles.length = 0; //clear old articles
 
-app.get('/',function(req, res){
-    // also res.send("");
-    res.json("You have hit the homepage URL");
-  
-})
-app.get('/news',(req, res)=>{ //also a way to write the .get
-    axios.get('https://www.bbc.com/')
-    //since it's promised based(asynchronous programming)
-    .then ((resp) => {
-        const HTML = resp.data;
-        //console.log(HTML);
-        const $ = cheerio.load(HTML);
+      $('a:contains("tech")', HTML).each(function () {
+        const title = $(this).text(); // $(this) means  $('a:contains("tech")',html)
+        const url = $(this).attr("href");
 
-        $('a:contains("tech")',HTML).each(function(){
-            const title = $(this).text(); // $(this) means  $('a:contains("tech")',html)
-            const url = $(this).attr('href');
+        //in the array articles we push an object with title and url
+        articles.push({
+          title,
+          url,
+        });
+      });
 
-            //in the array articles we push an object with title and url
-            articles.push({
-                title,
-                url
-            })
-            res.json(articles);
-        })
-    }).catch((err)=>(console.error(err)))
-})
+      // Save articles to a file after modification
+      fs.writeFile(
+        "./data/articles.json",
+        JSON.stringify(articles, null, 2),
+        (err) => {
+          if (err) {
+            console.error("Error writing file", err);
+          } else {
+            console.log("Articles saved to articles.json");
+          }
+        }
+      );
 
-//call the router
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}, access with "http://localhost:${PORT}/"`));
+      res.json(articles);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running: "http://localhost:${PORT}"`)
+);
