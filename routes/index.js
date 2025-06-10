fa5f84f6a4592dedf9f85651a49b253eafebfc4c
Benjamin Chege <benjamwa05@gmail.com>
2025-06-10 21:32:18 +0300

    npm run from diff starting points

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const  router = express.Router();
 
+let website;
+
 
 router.get('/', (req, res) => {
   res.render('index', { title: "Scrap" });
 });
 router.post('/submit', (req, res) => {
-  const {website} = req.body
+  website = req.body
   res.render('toServer.ejs', {website})
+  return website
 })
+
 
 module.exports = router
